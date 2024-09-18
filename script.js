a1303cc096ac486277eb215f862c4e51b6f05539
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-19 00:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+function moveFocus(current, event) {
+            if (current.value.length >= 1) {
+                // Move focus to the next input
+                const inputs = Array.from(document.querySelectorAll('.code'));
+                const currentIndex = inputs.indexOf(current);
+                if (currentIndex < inputs.length - 1) {
+                    inputs[currentIndex + 1].focus();
+                }
+            }
+        }
+
+function handleBackspace(current, event) {
+            if (event.key === 'Backspace') {
+                event.preventDefault();
+                if (current.value.length === 0) {
+                    const inputs = Array.from(document.querySelectorAll('.code'));
+                    const currentIndex = inputs.indexOf(current);
+                    if (currentIndex > 0) {
+                        const previousInput = inputs[currentIndex - 1];
+                        previousInput.focus();
+                        previousInput.value = '';
+                    }
+                } else {
+                    current.value = '';
+                }
+            }
+        }
